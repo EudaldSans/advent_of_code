13fc849814bf5fada91cdaa0d4e4d9dbcd1d8351
Eudald Sans <eudald.sans@gmail.com>
2021-12-06 13:43:20 +0100

    Add puzzle 2 for 2021 day 4 in puzzles.py

--- 2021_day_04/puzzles.py
+++ 2021_day_04/puzzles.py
@@ -9,18 +9,24 @@ class BingoBoard:
         self._number_track = np.zeros(board.shape, dtype=int)
         self._number_track.flags.writeable = True
 
+        self.won = False
+        self._last_draw = 0
+
     def new_number_drawn(self, number: int) -> bool:
         self._number_track[self._board == number] = 1
 
         completed_rows = np.prod(self._number_track, axis=0)
         completed_columns = np.prod(self._number_track, axis=1)
 
-        return 1 in completed_columns or 1 in completed_rows
+        self.won = (1 in completed_columns or 1 in completed_rows)
+        self._last_draw = number
+
+        return self.won
 
     def find_score(self) -> int:
         numbers_not_drawn = self._board - self._board*self._number_track
 
-        return np.sum(numbers_not_drawn)
+        return int(np.sum(numbers_not_drawn)) * self._last_draw
 
     def __str__(self):
         return str(self._board)
@@ -57,20 +63,38 @@ def main(path_to_bingo):
         boards.append(new_board)
 
     winner_board = None
+    loser_board = None
+    boards_that_won = 0
+    draw = -1
+
     for draw in number_draw:
         for board in boards:
+            if board.won:
+                continue
+
             if board.new_number_drawn(draw):
-                winner_board = board
+                boards_that_won += 1
+
+                if winner_board is None:
+                    winner_board = board
+
+            if boards_that_won >= len(boards):
+                loser_board = board
                 break
 
-        if winner_board is not None:
-            break
+    print(f'Last number drawn: {draw}')
 
     print(f'Winner board: ')
     print('-----------------')
     print(winner_board)
     print('-----------------')
-    print(f'Score: {winner_board.find_score()*draw}')
+    print(f'Score: {winner_board.find_score()}')
+
+    print(f'Loser board: ')
+    print('-----------------')
+    print(loser_board)
+    print('-----------------')
+    print(f'Score: {loser_board.find_score()}')
 
 
 if __name__ == '__main__':
